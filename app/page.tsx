c040615f2cdb265f233f4f8239aebcb75745d611
Srhiulli <srhiulli@gmail.com>
2025-02-02 17:45:08 -0300

    feat: add more items to quick search

--- app/page.tsx
+++ app/page.tsx
@@ -30,8 +30,8 @@ const Home = async () => {
           </Button>
         </div>
 
-        <div className="flex gap-3 mt-6">
-          <Button className="gap-2" variant="secondary">
+        <div className="flex gap-3 mt-6 overflow-auto [&::-webkit-scrollbar]:hidden">
+          <Button className="gap-2 " variant="secondary">
             <Image src="/cabelo.svg" width={16} height={16} alt="Cabelo" />
             Cabelo
           </Button>
@@ -47,6 +47,28 @@ const Home = async () => {
               alt="Acabamento"
             />
             Acabamento
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <Image
+              src="/sobrancelha.svg"
+              width={16}
+              height={16}
+              alt="Sobrancelha"
+            />
+            Sobrancelha
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <Image src="/massagem.svg" width={16} height={16} alt="Massagem" />
+            Massagem
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <Image
+              src="/hidratacao.svg"
+              width={16}
+              height={16}
+              alt="Hidratação"
+            />
+            Hidratação
           </Button>
         </div>
 
