a57c257437dd3584fe20b31c118dd2733c5f226d
Srhiulli <srhiulli@gmail.com>
2025-02-02 15:43:48 -0300

    feat: add static images

---
+++ app/_components/header.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent } from "./ui/card";
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent>
+        <h1>Header</h1>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Header from "./_components/header";
 
 export default function Home() {
   return (
-   <h1 className="text-red-100 mb-5"> Hello </h1>
+    <div>
+      <Header />
+    </div>
   );
 }
