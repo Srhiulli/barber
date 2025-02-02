0c417b7b670ab7df77fe1507fb03f87978486993
Srhiulli <srhiulli@gmail.com>
2025-02-02 16:10:02 -0300

    feat: add more to home

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,10 +1,28 @@
+import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
+import { MenuIcon } from "lucide-react";
+// import { Sheet, SheetTrigger } from "./ui/sheet"
+// import SidebarSheet from "./sidebar-sheet"
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
     <Card>
-      <CardContent>
-        <h1>Header</h1>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Link href="/">
+          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        </Link>
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+
+        {/* <Sheet>
+          <SheetTrigger asChild>
+           
+          </SheetTrigger>
+          <SidebarSheet />
+        </Sheet> */}
       </CardContent>
     </Card>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
+import { SearchIcon } from "lucide-react";
 import Header from "./_components/header";
+import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold"> Olá, Sarinha</h2>
+        <p>Domingo, 02 de fevereiro</p>
+        <div className="flex items-center gap-2 mt-8">
+          <Input placeholder="Faça a sua busca" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative w-full h-[150px] mt-6 ">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="object-cover rounded-xl"
+            alt="banner"
+          />
+        </div>
+      </div>
     </div>
   );
 }
