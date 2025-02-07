adaf99604a3ad4506d2770d7fb8845925bab58d0
Srhiulli <srhiulli@gmail.com>
2025-02-06 21:55:02 -0300

    feat: add google button to login

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,18 +1,54 @@
 "use client";
 
 import { Button } from "./ui/button";
-import { CalendarIcon, HomeIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon } from "lucide-react";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 import { quickSearchOptions } from "../_constants/search";
 import Link from "next/link";
 import Image from "next/image";
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 const SidebarSheet = () => {
   return (
     <SheetContent className="overflow-y-auto">
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
+
+      <div className="flex items-center justify-between gap-3 border-solid">
+        <h2 className="font-bold"> Olá, faça seu login </h2>
+        <Dialog>
+          <DialogTrigger>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[60%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-2 font-bold">
+              <Image
+                src="/google.svg"
+                alt="google logo"
+                width={18}
+                height={18}
+              />{" "}
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+      </div>
+
       <div className="flex flex-col gap-2 border-b border-solid py-5">
         <SheetClose asChild>
           <Button className="justify-start gap-2" variant="ghost" asChild>
