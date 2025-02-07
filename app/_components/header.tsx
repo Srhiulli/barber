6b75ad383a7b511f1e68fed519d0823b8eaf24f4
Srhiulli <srhiulli@gmail.com>
2025-02-06 21:17:28 -0300

    feat: add sidebar

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
-import { MenuIcon } from "lucide-react";
-// import { Sheet, SheetTrigger } from "./ui/sheet"
-// import SidebarSheet from "./sidebar-sheet"
-import Link from "next/link";
 import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetTrigger } from "./ui/sheet";
+import SidebarSheet from "./sidebar-sheet";
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -13,16 +13,15 @@ const Header = () => {
         <Link href="/">
           <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
         </Link>
-        <Button size="icon" variant="outline">
-          <MenuIcon />
-        </Button>
 
-        {/* <Sheet>
+        <Sheet>
           <SheetTrigger asChild>
-           
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
           </SheetTrigger>
           <SidebarSheet />
-        </Sheet> */}
+        </Sheet>
       </CardContent>
     </Card>
   );
---
+++ app/_components/sidebar-sheet.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { CalendarIcon, HomeIcon } from "lucide-react";
+import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
+import { quickSearchOptions } from "../_constants/search";
+import Link from "next/link";
+import Image from "next/image";
+
+const SidebarSheet = () => {
+  return (
+    <SheetContent className="overflow-y-auto">
+      <SheetHeader>
+        <SheetTitle className="text-left">Menu</SheetTitle>
+      </SheetHeader>
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        <SheetClose asChild>
+          <Button className="justify-start gap-2" variant="ghost" asChild>
+            <Link href="/">
+              <HomeIcon size={18} />
+              Início
+            </Link>
+          </Button>
+        </SheetClose>
+        <Button className="justify-start gap-2" variant="ghost" asChild>
+          <Link href="/bookings">
+            <CalendarIcon size={18} />
+            Agendamentos
+          </Link>
+        </Button>
+      </div>
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        {quickSearchOptions.map((option) => (
+          <SheetClose key={option.title} asChild>
+            <Button className="justify-start gap-2" variant="ghost" asChild>
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  alt={option.title}
+                  src={option.imageUrl}
+                  height={18}
+                  width={18}
+                />
+                {option.title}
+              </Link>
+            </Button>
+          </SheetClose>
+        ))}
+      </div>
+    </SheetContent>
+  );
+};
+
+export default SidebarSheet;
